6917369303eadea84f23b74e95775539f03899bc
Nazila <103484801+Nazila001@users.noreply.github.com>
2023-02-23 14:17:40 +0330

    creating file

---
+++ redirect.ts
@@ -0,0 +1,20 @@
+
+import { UserModel } from './view/user-model';
+import { UserAuthentication } from './valid/info-valid';
+
+
+
+{
+    let element = (document.getElementById("redirect")as any);
+    element.onclick = ()=>{
+        let userValidation  = new UserAuthentication({
+            username : 'a@b.com',
+            pass : '12345'
+        });
+        if (userValidation.validUserE())
+            window.location.href = '/index5.html';
+        else return alert("error")
+            // else return "Error"
+
+    }
+}
