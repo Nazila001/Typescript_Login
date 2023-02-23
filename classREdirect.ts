9787bd8885a63998317939eaa31ce57dfa79a910
Nazila <103484801+Nazila001@users.noreply.github.com>
2023-02-23 14:09:48 +0330

    creating file

---
+++ classREdirect.ts
@@ -0,0 +1,16 @@
+// class Click {   
+
+//     link : string;
+
+//     linkOfRedirect () :boolean {
+//         window.location.href = link; 
+//         return true
+        
+//     }
+
+//     redirectByClick() : boolean {
+//         let element = document.getElementById("redirect");
+//         element.onclick = linkOfRedirect(); 
+//         return true
+//     };
+// }
